acc516788664b1768676988b66867640b08bb164
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-04-26 11:44:48 +0700

    mongo-db crud v1

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 const methods = {}
+var mongo = require('mongodb');
 
 methods.getAllBook = (req, res, next) => {
 
@@ -6,8 +7,8 @@ methods.getAllBook = (req, res, next) => {
     var url = 'mongodb://localhost:27017/library';
     MongoClient.connect(url, function(err, db) {
 
-        db.collection('books').find({}).toArray(function(err,docs){
-            if(err){
+        db.collection('books').find({}).toArray(function(err, docs) {
+            if (err) {
                 res.json(err)
             } else {
                 res.json(docs)
@@ -17,19 +18,66 @@ methods.getAllBook = (req, res, next) => {
     });
 }
 
-methods.insertBook = (req,res,next) => {
+methods.insertBook = (req, res, next) => {
     var MongoClient = require('mongodb').MongoClient
     var url = 'mongodb://localhost:27017/library';
     MongoClient.connect(url, function(err, db) {
 
         db.collection('books').insertOne({
-            "isbn":req.body.isbn,
-            "title":req.body.title,
-            "author":req.body.author,
-            "category":req.body.catgeory,
-            "stock":req.body.stock
-        },(err, book) => {
-            if(err){
+            "isbn": req.body.isbn,
+            "title": req.body.title,
+            "author": req.body.author,
+            "category": req.body.category,
+            "stock": req.body.stock
+        }, (err, book) => {
+            if (err) {
+                res.json(err)
+            } else {
+                res.json(book)
+            }
+            db.close()
+        })
+    });
+}
+
+methods.updateBook = (req, res, next) => {
+    // res.json(req.body.title);
+    var MongoClient = require('mongodb').MongoClient
+    var url = 'mongodb://localhost:27017/library';
+
+    MongoClient.connect(url, function(err, db) {
+
+        db.collection('books').updateOne({
+            "_id": new mongo.ObjectID(req.params.id)
+        }, {
+            $set: {
+                "isbn": req.body.isbn,
+                "title": req.body.title,
+                "author": req.body.author,
+                "category": req.body.category,
+                "stock": req.body.stock
+            }
+        }, (err, book) => {
+            if (err) {
+                res.json(err)
+            } else {
+                res.json(book)
+            }
+            db.close()
+        })
+    });
+}
+
+methods.deleteBook = (req,res,next) => {
+    var MongoClient = require('mongodb').MongoClient
+    var url = 'mongodb://localhost:27017/library';
+
+    MongoClient.connect(url, function(err, db) {
+
+        db.collection('books').deleteOne({
+            "_id": new mongo.ObjectID(req.params.id)
+        }, (err, book) => {
+            if (err) {
                 res.json(err)
             } else {
                 res.json(book)
--- routes/books.js
+++ routes/books.js
@@ -9,7 +9,7 @@ const bookController = require('../controllers/bookController');
 
 router.get('/books',bookController.getAllBook);
 router.post('/books/',bookController.insertBook);
-// router.put('/books/:id',bookController.updateBook);
-// router.delete('/books/:id',bookController.deleteBook);
+router.put('/books/:id',bookController.updateBook);
+router.delete('/books/:id',bookController.deleteBook);
 
 module.exports = router;
