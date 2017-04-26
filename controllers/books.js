1a7dd73564f53a8857beb64e76f66ce7a0763f4f
diditaditya <di2t.aditya@gmail.com>
2017-04-26 22:37:20 +0700

    Fixes the controller for updating book info

--- controllers/books.js
+++ controllers/books.js
@@ -52,23 +52,29 @@ let Book = {
     });
   },
   update: (req, res) => {
-    let isbn = req.body.isbn;
-    let title = req.body.title;
-    let author = req.body.author;
-    let category = req.body.category;
-    let stock = req.body.stock;
     let bookId = new ObjectId(req.params.id);
     MongoClient.connect(url, (err, db) => {
       let books = db.collection('books');
-      books.updateOne({'_id':bookId}, {$set: {
-        isbn:isbn,
-        title:title,
-        author:author,
-        category:category,
-        stock:stock
-      }}, (err, result) => {
-        if (result) {
-          res.json(result);
+      books.find({'_id':bookId}).toArray((err, book) => {
+        if (book) {
+          let isbn = req.body.isbn || book[0].isbn;
+          let title = req.body.title || book[0].title;
+          let author = req.body.author || book[0].author;
+          let category = req.body.category || book[0].category;
+          let stock = req.body.stock || book[0].stock;
+          books.updateOne({'_id':bookId}, {$set: {
+            isbn:isbn,
+            title:title,
+            author:author,
+            category:category,
+            stock:stock
+          }}, (err, result) => {
+            if (result) {
+              res.json(result);
+            } else {
+              res.send(err);
+            }
+          });
         } else {
           res.send(err);
         }
