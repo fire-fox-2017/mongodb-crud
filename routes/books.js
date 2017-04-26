5cc37d373b558d44025b78af41ce4abba8a5b32f
haryanawisnu <arya.wisnu05@gmail.com>
2017-04-26 10:48:13 +0700

    add file books

---
+++ routes/books.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var controllers = require('../controllers/booksControllers');
+
+/* GET home page. */
+//router.get('/', );
+
+module.exports = router;
